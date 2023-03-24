81762809133ddcf3d6b552f88da77ba60671b1c8
CtripleU <cuoyemike@gmail.com>
2023-03-24 17:37:19 +0200

    more fixes

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,18 +7,16 @@ from models import storage
 
 app = Flask(__name__)
 
-
-@app.teardown_appcontext
-def delete_SQLA(self):
-    """Remove the current SQLAlchemySession"""
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def display_HTML():
     """Display html page with States"""
     states = storage.all('State')
     return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def delete_SQLA(self):
+    """Remove the current SQLAlchemySession"""
+    storage.close()
 
 
 if __name__ == '__main__':
